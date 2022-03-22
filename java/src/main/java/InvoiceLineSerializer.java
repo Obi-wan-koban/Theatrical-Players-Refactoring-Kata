22e6cf3d8544eed936a049f30f8bfdc5ac59013d
Obi-wan-koban <kobe.roosens@gmail.com>
2022-03-22 21:51:54 +0100

    with json writer

---
+++ java/src/main/java/InvoiceLineSerializer.java
@@ -0,0 +1,19 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import java.lang.reflect.Type;
+
+public class InvoiceLineSerializer implements JsonSerializer<InvoiceLine> {
+    @Override
+    public JsonElement serialize(InvoiceLine src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject json = new JsonObject();
+        json.addProperty( "playName", src.getPlayName());
+        String formattedAmount = InvoiceLineWriter.frmt.format(src.getAmount() / 100);
+        json.addProperty("amount", formattedAmount);
+        json.addProperty("audience", src.getAudience());
+        json.addProperty( "volumeCredits", src.getVolume());
+        return json;
+    }
+}
