60856fda4273b89403fbdbedb851a9663b04b9b7
Obi-wan-koban <kobe.roosens@gmail.com>
2022-03-22 20:32:07 +0100

    So that's one

--- java/src/main/java/PerformancePlayCalculatorTragedy.java
+++ java/src/main/java/PerformancePlayCalculatorTragedy.java
@@ -1,9 +1,7 @@
 public class PerformancePlayCalculatorTragedy extends PerformancePlayCalculator {
-    InvoiceLineWriter writer;
 
     public PerformancePlayCalculatorTragedy(Play play, Performance performance) {
         super(play, performance);
-        writer = new InvoiceLineWriter(this);
     }
 
     @Override
@@ -20,16 +18,4 @@ public class PerformancePlayCalculatorTragedy extends PerformancePlayCalculator 
         return Math.max(getPerformance().audience - 30, 0);
     }
 
-    public InvoiceLineWriter getWriter() {
-        return writer;
-    }
-
-    public void setWriter(InvoiceLineWriter writer) {
-        this.writer = writer;
-    }
-
-    @Override
-    public String toString() {
-        return writer.toString();
-    }
 }
